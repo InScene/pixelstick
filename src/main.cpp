c64c445573c86cb1f03ebbd2ff39e33b899c96ec
InScene <mertens.chr@gmail.com>
2022-08-17 19:04:09 +0200

    build: first buildable version (#3)
    * build: add needed arduino libraries
    
    * refactor: make it compileable
    
    * chore: add name to file description

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 /*
   DIY Lighty + SD + OLED + 5 way joystick + WS2812B RGB LED
-  Dirk Essl 2020
+  Christian Mertens 2022
+  based on pixelstick by Dirk Essl 2020
   based on Digital Light Wand by Michael Ross
 
   Lighty is for use in specialized Light Painting Photography
@@ -27,12 +28,7 @@
 */
 
 // Library initialization
-#include <Adafruit_NeoPixel.h>           // Library for the WS2812 Neopixel Strip
-#include <SD.h>                          // Library for the SD Card
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include "main.h"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -61,8 +57,6 @@ int repeatTimes = 1;                      // Variable to keep track of number of
 int brightness = 50;                      // Variable and default for the Brightness of the strip
 
 // EEPROM setup. saves all values in eeprom. the mega has 4kb of eeprom storage
-
-#include <EEPROM.h>
 // the current address in the EEPROM (i.e. which byte we're going to write to next)
 
 // set addresses for save
@@ -632,27 +626,4 @@ void isort(String * filenames, int n) {
     }
     filenames[k + 1] = j;
   }
-}
-
-PROGMEM const unsigned char gammaTable[]  = {
-  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,
-  2,  2,  2,  2,  2,  3,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,
-  4,  4,  4,  4,  5,  5,  5,  5,  5,  6,  6,  6,  6,  6,  7,  7,
-  7,  7,  7,  8,  8,  8,  8,  9,  9,  9,  9, 10, 10, 10, 10, 11,
-  11, 11, 12, 12, 12, 13, 13, 13, 13, 14, 14, 14, 15, 15, 16, 16,
-  16, 17, 17, 17, 18, 18, 18, 19, 19, 20, 20, 21, 21, 21, 22, 22,
-  23, 23, 24, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28, 29, 29, 30,
-  30, 31, 32, 32, 33, 33, 34, 34, 35, 35, 36, 37, 37, 38, 38, 39,
-  40, 40, 41, 41, 42, 43, 43, 44, 45, 45, 46, 47, 47, 48, 49, 50,
-  50, 51, 52, 52, 53, 54, 55, 55, 56, 57, 58, 58, 59, 60, 61, 62,
-  62, 63, 64, 65, 66, 67, 67, 68, 69, 70, 71, 72, 73, 74, 74, 75,
-  76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91,
-  92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 104, 105, 106, 107, 108,
-  109, 110, 111, 113, 114, 115, 116, 117, 118, 120, 121, 122, 123, 125, 126, 127
-};
-
-inline byte gamma(byte x) {
-  return pgm_read_byte(&gammaTable[x]);
 }
