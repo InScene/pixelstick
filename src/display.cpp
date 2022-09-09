c37313a2051aa46ae51eac73003f73511d52e771
Christian Mertens <mertens.chr@gmail.com>
2022-09-10 00:50:26 +0200

    refactor: rework display output

--- src/display.cpp
+++ src/display.cpp
@@ -14,7 +14,7 @@ Display::~Display()
   
 }
 
-void Display::set(const String& header, const String& text, const int value)
+void Display::set(const String& header, const String& text, const String& text2, const uint16_t value)
 {
   lcd.clearDisplay();
   lcd.setTextSize(2);
@@ -23,6 +23,23 @@ void Display::set(const String& header, const String& text, const int value)
   lcd.println(header);
   lcd.setTextSize(1);
   lcd.println(text);
+  lcd.println(text2);
+  lcd.print(F("Value: "));
+  lcd.println(value);
+  lcd.display();
+}
+
+void Display::set(const String& header, const String& text, const String& text2, const unsigned long value)
+{
+  lcd.clearDisplay();
+  lcd.setTextSize(2);
+  lcd.setTextColor(SSD1306_WHITE);
+  lcd.setCursor(0, 0);
+  lcd.println(header);
+  lcd.setTextSize(1);
+  lcd.println(text);
+  lcd.println(text2);
+  lcd.print(F("Value: "));
   lcd.println(value);
   lcd.display();
 }
--- src/display.h
+++ src/display.h
@@ -12,13 +12,13 @@
 #define MENU_MAIN_FILE F("1:File ")
 
 #define MENU_FILE_HEADER F("1:File ")
-#define MENU_FILE_SELECT F("1_1:File Select ")
-#define MENU_FILE_BRIGHTNESS F("1_2:Brightness ")
-#define MENU_FILE_INIT_DELAY F("1_3:Init Delay ")
-#define MENU_FILE_FRAME_DELAY F("1_4:Frame Delay")
-#define MENU_FILE_REPEAT_TIMES F("1_5:Repeat Times")
-#define MENU_FILE_REPEAT_DELAY F("1_6:Repeat Delay")
-#define MENU_FILE_MAIN_MENU F("1_7:Main Menu")
+#define MENU_FILE_SELECT F("1:File Select ")
+#define MENU_FILE_BRIGHTNESS F("2:Brightness ")
+#define MENU_FILE_INIT_DELAY F("3:Init Delay ")
+#define MENU_FILE_FRAME_DELAY F("4:Frame Delay")
+#define MENU_FILE_REPEAT_TIMES F("5:Repeat Times")
+#define MENU_FILE_REPEAT_DELAY F("6:Repeat Delay")
+#define MENU_FILE_MAIN_MENU F("7:Main Menu")
 
 #define MENU_HEADER_NOW_PLAYING F(" ")
 #define MENU_NOW_PLAYING F("Now Playing")
@@ -29,7 +29,8 @@ class Display
     Display(Adafruit_SSD1306& lcd);
     ~Display();
 
-    void set(const String& header, const String& text, const int value);
+    void set(const String& header, const String& text, const String& text2, const uint16_t value);
+    void set(const String& header, const String& text, const String& text2, const unsigned long value);
     void set(const String& header, const String& text, const String& text2);
 
   private:
